098297a17de9ef2d28836939656eac1e6b3030d3
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2020-10-10 13:14:41 +0700

    docker-php-ext-install mysqli

--- tools/docker/starter/run.sh
+++ tools/docker/starter/run.sh
@@ -9,3 +9,5 @@ export LD_PRELOAD="/usr/lib/preloadable_libiconv.so php"
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 composer install --no-ansi --no-interaction --no-progress --optimize-autoloader --prefer-dist
+
+docker-php-ext-install mysqli
