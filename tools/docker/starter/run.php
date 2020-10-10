d9aa58b9533e2a8282d5b3bf74422636f6b08017
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2020-10-10 15:48:38 +0700

    log

--- tools/docker/starter/run.php
+++ tools/docker/starter/run.php
@@ -1,10 +1,16 @@
 #!/usr/bin/env php
 <?php
 
+namespace Wpt;
+
+use mysqli;
+
+log('Installing vendors');
 echo shell_exec('composer install --ansi --no-interaction --no-progress --optimize-autoloader --prefer-dist');
 
 // echo shell_exec('cd db && ../vendor/bin/ruckus.php db:migrate');
 
+log('Creating DB and user');
 $mysqli = new mysqli('db', 'root', '123456');
 
 $DB_CHARSET = getenv('DB_CHARSET') ?: 'utf8';
@@ -15,3 +21,9 @@ $mysqli->query('GRANT USAGE ON wpti.* TO wpti');
 $mysqli->query('DROP USER wpti');
 $mysqli->query('CREATE USER wpti IDENTIFIED BY "wpti"');
 $mysqli->query('GRANT ALL ON wpti.* TO wpti');
+
+
+function log($message) {
+    $now = date(DATE_ATOM);
+    echo "[$now] $message\n";
+}
