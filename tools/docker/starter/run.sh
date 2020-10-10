587637965ac0e3d79b6b79eadb44341c529cfa62
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2020-10-10 13:44:17 +0700

    migrate

--- tools/docker/starter/run.sh
+++ tools/docker/starter/run.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env sh
 
-apk add gnu-libiconv --update-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/community/ --allow-untrusted
-
-apk add --no-cache git
-
-export LD_PRELOAD="/usr/lib/preloadable_libiconv.so php"
-
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-
 composer install --no-ansi --no-interaction --no-progress --optimize-autoloader --prefer-dist
-
-docker-php-ext-install mysqli
